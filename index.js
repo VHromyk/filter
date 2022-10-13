c718529d3ecdc7d8daf586faa61a87167a1b733e
VHromyk <108476400+dev-smart-ui@users.noreply.github.com>
2022-10-14 01:22:44 +0300

    add some fixing

--- index.js
+++ index.js
@@ -11,40 +11,32 @@ function filterGoods() {
     const camerasCheck = cameras[0] === 'all' ? [] : cameras;
 
 const filteredEl = DATA.filter(
-    (n) => {
-        console.log(cameras);
-        return (
+    (n) => (
             (!sizes.length || sizes.includes(n.size)) &&
             (!camerasCheck.length || camerasCheck.includes(n.camera)) &&
             (!brands.length || brands.includes(n.brand))
-        )
-    }
-)
+        ))
 
     outputGoods(filteredEl);
 }
 
-function outputGoods(goods) {
 
-    console.log(goods);
-    document.getElementById("goods").innerHTML = goods
-        .map(
-            (good) => `
-    <div class="single-goods">
-    <div class="border-2 border-gray-200 rounded-lg overflow-hidden mb-3">
+const createMarkup = (good) => `
+    <div class="single-goods border-2 border-gray-200 rounded-lg overflow-hidden m-3">
       <div class="text-center">
       <h3 class"py-2 font-bold">Brand: ${good.brand}</h3>
-      <p>Name: ${good.name}</p>
       <p>Size: ${good.size}</p>
       <p> Camera: ${good.camera}</p>
-      <div class="overflow-hidden flex justify-center p-8">
-</div>
       </div>
       </div>
-    </div>
   `
-        )
-        .join("");
+
+function outputGoods(goods) {
+    const goodsLink = document.querySelector('#goods');
+
+    const goodsList = goods.map(good => createMarkup(good)).join('');
+
+    goodsLink.innerHTML = goodsList;
 }
 
 const DATA = [
